c8a1cd89b085745321d4a2720481e8f26365d032
0xeps <5b0g0034@stust.edu.tw>
2024-05-06 20:10:19 +0800

    update

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import logo from './logo.svg';
+import './App.css';
+import MySlider from './MySlider';
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p className="Name">
+          STUST <code>5B0G0034 許曜丞</code> upload
+        </p>
+        
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn How Not to Fail This Class
+        </a>
+
+        <section className="Work-react">
+
+          <div>
+            <h5>第1個元件</h5>
+            <MySlider />
+          </div>
+
+          <div>
+            <h5>第2個元件</h5>
+            <MySlider />
+          </div>
+
+          <div>
+            <h5>第3個元件</h5>
+            <MySlider />
+          </div>
+
+        </section>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
