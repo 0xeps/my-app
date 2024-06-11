0c8b44ca6bdcaa196c1c81df756463d4ea7fd0e0
Hsu, Yao-Cheng <145269966+0xeps@users.noreply.github.com>
2024-06-11 16:32:49 +0800

    tidy

--- src/App.js
+++ src/App.js
@@ -1,1 +1,23 @@
-import "./App.css"; import MyRGBPanel from "./components/MyRGBPanel"; import MySlider from "./components/MySlider"; function App() { return (<div className="App"><h1>MySlider</h1><MySlider /><h1>MyRGBPanel</h1><MyRGBPanel /></div>); } export default App;
+import "./App.css";
+import MyRGBPanel from "./components/rgb-panel/MyRGBPanel"
+import MySlider from "./components/rgb-panel/MySlider";
+import MyCalculator from "./components/calculator/MyCalculator";
+import TicTacToe from "./components/tic-tac-toe/TicTacToe";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>*** 5b0g0034 | 許曜丞 ***</h1>
+      <h1>MySlider</h1>
+      <MySlider />
+      <h1>MyRGBPanel</h1>
+      <MyRGBPanel />
+      <h1>MyCalculator</h1>
+      <MyCalculator />
+      <h1>Tic-Tac-Toe Game</h1>
+      <TicTacToe />
+    </div>
+  );
+}
+
+export default App;
